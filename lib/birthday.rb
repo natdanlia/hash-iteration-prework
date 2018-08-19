402603b60618bffbcd34044668fbffa599880602
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-08-19 21:56:42 +0000

    automatically backed up by learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -6,7 +6,7 @@
 
 def happy_birthday(birthday_kids)
   # add your code snippet here!
-  birthday_kids.each do | kid, age |
+  birthday_kids.collect do | kid, age |
     puts "Happy Birthday #{kid}! You are now #{age} years old!"
   end
 end
